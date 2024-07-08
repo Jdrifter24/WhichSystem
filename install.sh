6538716fa0cb9b9d4726cf772c73999f584e4052
Jose Miguel Ramos vega <135689234+Jdrifter24@users.noreply.github.com>
2024-07-08 13:58:33 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Paso 1: Clonar el repositorio de GitHub
+git clone https://github.com/Jdrifter24/WhichSystem.git
+
+# Paso 2: Cambiar al directorio del repositorio clonado
+cd WhichSystem
+
+# Paso 3: Hacer ejecutable el archivo WhichSystem.py
+chmod +x WhichSystem.py
